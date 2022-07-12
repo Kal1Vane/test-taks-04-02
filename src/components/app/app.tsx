6db21552245c4bb41406acd1546a737db58a8da7
Kirill Davletshin <prizrak41411@gmail.com>
2022-07-12 18:01:00 +0500

    add tailwindCSS

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        init TailwindCSS 
+      </h1>
+    </div>
+  );
+}
+
+export default App;
